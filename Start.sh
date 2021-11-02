d416c2f52b83ef60a40928b7a38c1fd67efaea31
FireGen <firegen.loading@gmail.com>
2021-11-03 01:52:52 +0200

    YEP MORE

--- Start.sh
+++ Start.sh
@@ -1,3 +1,4 @@
 sudo apt install build-essential
 make setup
+make clean
 @echo use "make build" to compile the kernel
--- build/Setup.sh
+++ build/Setup.sh
@@ -9,7 +9,7 @@ sudo apt install libgmp3-dev
 sudo apt install libmpc-dev
 sudo apt install libmpfr-dev
 sudo apt install texinfo
-@echo "building deps..."
+echo "building deps..."
 
 ORIGIN="$PWD"
 utils="$ORIGIN/binutils"
@@ -17,7 +17,7 @@ export PREFIX="$ORIGIN/i386elfgcc"
 export TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
 
-@echo "getting binutil sources..."
+echo "getting binutil sources..."
 mkdir "$utils"
 mkdir "$utils/src"
 
@@ -26,14 +26,14 @@ curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.gz
 tar xf binutils-2.35.1.tar.gz
 cd "$utils"
 
-@echo "compiling binutils..."
+echo "compiling binutils..."
 
 mkdir "$utils/binutils-build"
 cd "$utils/binutils-build"
 ../binutils-2.35.1/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
 sudo make all install 2>&1 | tee make.log
 
-@echo "getting gcc source..."
+echo "getting gcc source..."
 cd "$utils"
 curl -O https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
 tar xf gcc-10.2.0.tar.gz
@@ -41,19 +41,20 @@ tar xf gcc-10.2.0.tar.gz
 mkdir "$utils/gcc-build"
 cd "$utils/gcc-build"
 
-echo Configure
+echo "Configure"
 ../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c++ --without-headers
-@echo "rebuilding gcc"
-echo MAKE ALL-GCC:
+echo "rebuilding gcc"
+echo "MAKE ALL-GCC:"
 sudo make all-gcc
 
-echo MAKE ALL-TARGET-LIBGCC:
+echo "MAKE ALL-TARGET-LIBGCC:"
 sudo make all-target-libgcc
 
-echo MAKE INSTALL-GCC:
+echo "MAKE INSTALL-GCC:"
 sudo make install-gcc
 
-echo MAKE INSTALL-TARGET-LIBGCC
+echo "MAKE INSTALL-TARGET-LIBGCC"
 sudo make install-target-libgcc
 
-export PATH=$PATH:/usr/local/i386elfgcc/bin
+echo "Exporting path $PREFIX"
+export PATH="$PATH:$PREFIX"
