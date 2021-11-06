10556da0b9ff34ca1763871d4d9eb06b846b09c3
FireGen <firegen.loading@gmail.com>
2021-11-06 20:28:58 +0200

    fixed setup and hello kernel test

--- build/Setup.sh
+++ build/Setup.sh
@@ -1,4 +1,5 @@
-@echo "Installing deps..."
+echo "-------------------------------------"
+echo "Installing deps..."
 sudo apt install nasm
 
 sudo apt update
@@ -9,52 +10,66 @@ sudo apt install libgmp3-dev
 sudo apt install libmpc-dev
 sudo apt install libmpfr-dev
 sudo apt install texinfo
+echo "-------------------------------------"
 echo "building deps..."
 
 export TARGET=i386-elf
 START="$PWD"
 BINUTILS="$START/binutils"
+BINSRC="$BINUTILS/src"
 export GCC="$START/i386elfgcc"
 export PATH="$PATH:$GCC/bin"
-
+export PATH
+echo "-------------------------------------"
+echo $PATH
+echo "-------------------------------------"
 echo "getting binutil sources..."
 mkdir "$BINUTILS"
-mkdir "$BINUTILS/src"
+mkdir "$BINSRC"
 
-cd "$BINUTILS/src"
+cd "$SRCc"
 curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.gz
+echo "decompressing binutils source..."
 tar xf binutils-2.35.1.tar.gz
-cd "$BINUTILS"
 
+echo "-------------------------------------"
 echo "compiling binutils..."
 
 mkdir "$BINUTILS/binutils-build"
 cd "$BINUTILS/binutils-build"
-../binutils-2.35.1/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$GCC 2>&1 | tee configure.log
+
+$BINSRC/binutils-2.35.1/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$GCC 2>&1 | tee configure.log
 sudo make all install 2>&1 | tee make.log
 
+echo "-------------------------------------"
 echo "getting gcc source..."
-cd "$BINUTILS"
+cd "$BINSRC"
+
 curl -O https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
+echo "decompressing gcc source..."
 tar xf gcc-10.2.0.tar.gz
 
 mkdir "$BINUTILS/gcc-build"
 cd "$BINUTILS/gcc-build"
-
+echo "-------------------------------------"
 echo "Configure"
-../gcc-10.2.0/configure --target=$TARGET --prefix="$GCC" --disable-nls --disable-libssp --enable-language=c++ --without-headers
+$BINSRC/gcc-10.2.0/configure --target=$TARGET --prefix="$GCC" --disable-nls --disable-libssp --enable-language=c++ --without-headers
+echo "-------------------------------------"
 echo "rebuilding gcc"
+echo "-------------------------------------"
 echo "MAKE ALL-GCC:"
 sudo make all-gcc
-
+echo "-------------------------------------"
 echo "MAKE ALL-TARGET-LIBGCC:"
 sudo make all-target-libgcc
-
+echo "-------------------------------------"
 echo "MAKE INSTALL-GCC:"
 sudo make install-gcc
-
+echo "-------------------------------------"
 echo "MAKE INSTALL-TARGET-LIBGCC"
 sudo make install-target-libgcc
-
+echo "-------------------------------------"
 echo "Exporting path $GCC"
+echo "you might have to use the following command"
+echo "export PATH=\$PATH:$GCC/bin"
 export PATH="$PATH:$GCC/bin"
--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,18 @@
 static char *VGAptr = (char *)0xb8000;
 static int offset = 0;
 
-void main(){
-    char Hello[] = "Hello kernel";
-    print(Hello,sizeof(Hello[0])/sizeof(Hello));
-}
-
 void print(char* p, int size){
     for (int i = 0; i < size; i++)
-        VGAptr[offset++] = p[i];
+        VGAptr[offset++*2] = p[i];
 
     return ;
 }
+
+void main(){
+    char *Hello = "Hello kernel";
+    print(Hello,12);
+}
+
+
 
 
